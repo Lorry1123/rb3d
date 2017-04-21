58de0f7d54e030e0cba6f0924a12fffb9c7281c4
Lorry <Lorry1123@outlook.com>
2017-04-21 23:50:42 +0800

    ADD: test api

--- img_deal/hello.py
+++ img_deal/hello.py
@@ -1,8 +1,12 @@
-from flask import Flask,Blueprint,render_template
+from flask import Flask, Blueprint, render_template
+from img_deal.img_api import ImageReader
 
 IMG_DEAL_NAME = 'img_deal'
 
 img_deal = Blueprint('img_deal', __name__, template_folder="templates", static_folder="static")
+
+
+DEFAULT_IMAGE_PATH = 'img_deal/img_api/img/'
 
 
 @img_deal.route('/hello')
@@ -18,3 +22,12 @@ def index():
 @img_deal.route('/test')
 def test():
     return render_template('testPage.html')
+
+
+@img_deal.route('/img')
+def image():
+    img = ImageReader(path=DEFAULT_IMAGE_PATH)
+    # img.decode(DEFAULT_IMAGE_PATH)
+    img.calc_lov(size=3)
+    img.show_lov()
+    return 'success'
