1391aec86a7f204b9c6693b8f99c2afa2e15d4cb
Lorry <Lorry1123@outlook.com>
2017-04-27 15:50:16 +0800

    ADD: mongodb test api

--- img_deal/hello.py
+++ img_deal/hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, render_template
 from img_deal.img_api import ImageReader
+from img_deal.actions import user
 
 IMG_DEAL_NAME = 'img_deal'
 
@@ -26,10 +27,16 @@ def test():
 
 @img_deal.route('/img')
 def image():
-    img = ImageReader(path=DEFAULT_IMAGE_PATH, name='flower_2')
-    # img.save(path=DEFAULT_IMAGE_PATH, name='sophia_small')
+    img = ImageReader(path=DEFAULT_IMAGE_PATH, name='lena')
     # img.calc_lov(size=3)
     # img.show_lov(DEFAULT_IMAGE_PATH, name='zebra')
-    img.calc_deep_map(debug_mode=True, debug_path=DEFAULT_IMAGE_PATH, name='flower_2')
-    img.red_blue_translation(path=DEFAULT_IMAGE_PATH, name='flower_2')
+    img.calc_deep_map(debug_mode=True)
+    img.red_blue_translation()
     return 'success'
+
+
+@img_deal.route('/get_user')
+def get_user():
+    r = user.create_user({'uid': '1123', 'psw': '123456'})
+
+    return str(r)
