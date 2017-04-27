e353660ff13db96bdd0c7ffbd160b96297c0947a
Lorry <Lorry1123@outlook.com>
2017-04-27 21:11:06 +0800

    ADD: login supported

---
+++ frontend/js/pages/login.js
@@ -0,0 +1,60 @@
+/**
+ * Created by luorui on 2017/4/27.
+ */
+
+var LoginViewModel = function () {
+  self.inited = ko.observable(false);
+  self.login_switch = ko.observable(1);
+
+  self.uid = ko.observable('');
+  self.psw = ko.observable('');
+
+  self.r_uid = ko.observable('');
+  self.r_psw = ko.observable('');
+  self.r_psw_repeat = ko.observable('');
+  self.r_mobile = ko.observable('');
+
+  self.login = function (data, event) {
+    var api = '/img_deal/login_check';
+    var data = {uid: self.uid(), psw: self.psw()};
+    console.log(data);
+    $.post(api, data, function (res) {
+      if (res.status == 0) {
+        window.location.href = '/img_deal/index';
+      }
+      else {
+        console.log(res.error);
+      }
+    });
+  };
+
+  self.regist = function (data, event) {
+    if (self.r_psw() != self.r_psw_repeat()) {
+      alert('密码不一致');
+    }
+    var api = '/img_deal/regist';
+    var data = {uid: self.r_uid(), psw: self.r_psw(), mobile: self.r_mobile()};
+    $.post(api, data, function (res) {
+      if (res.status == 0) {
+        self.login_switch(-1 * self.login_switch());
+        alert('注册成功');
+      }
+      else {
+        alert('注册失败');
+      }
+    });
+
+  };
+
+  self.init = function () {
+    self.login_switch(1);
+
+
+    self.inited(true);
+  };
+
+  self.init();
+};
+
+ko.applyBindings(new LoginViewModel());
+module.exports = LoginViewModel;
--- img_deal/hello.py
+++ img_deal/hello.py
@@ -1,6 +1,7 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, jsonify
 from img_deal.img_api import ImageReader
 from img_deal.actions import user
+
 
 IMG_DEAL_NAME = 'img_deal'
 
@@ -35,8 +36,38 @@ def image():
     return 'success'
 
 
-@img_deal.route('/get_user')
-def get_user():
-    r = user.create_user({'uid': '1123', 'psw': '123456'})
+@img_deal.route('/login')
+def login():
+    return render_template('login.html')
 
-    return str(r)
+
+@img_deal.route('/login_check', methods=['GET', 'POST'])
+def login_check():
+    uid = request.values.get('uid')
+    psw = request.values.get('psw')
+
+    if not uid or not psw:
+        return jsonify(status=-1, error='uid or psw is empty')
+
+    r, li = user.find_user(dict(uid=uid, psw=psw))
+
+    print r, li
+
+    if not r:
+        return jsonify(status=0, msg='ok')
+    else:
+        return jsonify(status=-2, error='uid or psw is wrong')
+
+
+@img_deal.route('/regist', methods=['GET', 'POST'])
+def regist():
+    uid = request.values.get('uid')
+    psw = request.values.get('psw')
+    mobile = request.values.get('mobile')
+
+    if not uid or not psw or not mobile:
+        return jsonify(status=-1, error='some fields are empty')
+
+    r = user.create_user(dict(uid=uid, psw=psw, mobile=mobile))
+
+    return jsonify(status=r)
