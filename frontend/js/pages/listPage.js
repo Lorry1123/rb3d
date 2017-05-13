af49c6c448f6617924d63b62167853d920f4a0cd
Lorry <Lorry1123@outlook.com>
2017-05-13 09:51:44 +0800

    MOD: beautify list page

--- frontend/js/pages/listPage.js
+++ frontend/js/pages/listPage.js
@@ -23,13 +23,20 @@ var ListPageViewModel = function () {
       }
 
       var list_container = $("#image_list");
+      console.log(list_container);
       $.each(self.img_list(), function (index, element) {
-        var div1 = $('<div>');
+        var tr1 = $('<tr>');
+        var td1 = $('<td>');
+        var td2 = $('<td>');
+        var td3 = $('<td>');
         var img1 = $('<img>').attr('src', '../img_api/get_pic/' + element + '?t=' + Math.random());
         var img2 = $('<img>').attr('src', '../img_api/get_pic/' + element + '_3d?t=' + Math.random());
-        var a1 = $('<a>').attr('href', '../img_api/get_pic/' + element + '_3d').append('查看原图');
-        div1.append(img1).append(img2).append(a1);
-        list_container.append(div1);
+        var a1 = $('<a>').attr('href', '../img_api/get_pic/' + element + '_3d').append('查看大图');
+        td1.append(img1);
+        td2.append(img2);
+        td3.append(a1).attr('style','padding-top: 150px');
+        tr1.append(td1).append(td2).append(td3);
+        list_container.append(tr1);
       });
     });
 
